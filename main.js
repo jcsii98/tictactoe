590daa9b2e5690dea4f16288efc8b1564d3a0b32
jcsii98 <jose.c.saribong@gmail.com>
2023-04-17 20:33:37 +0800

    finalize js n css

--- main.js
+++ main.js
@@ -8,24 +8,18 @@ let gameBoard = [
 
 let gameHistory = [gameBoard];
 
-function checkWin() {
-  // check for horizontal wins
-  for (let i = 0; i < 3; i++) {
-    if (
-      gameBoard[i][0] === currentPlayer &&
-      gameBoard[i][1] === currentPlayer &&
-      gameBoard[i][2] === currentPlayer
-    ) {
-      return true;
-    }
-  }
+const cells = document.querySelectorAll(".cell");
 
-  // vertical wins
+function checkWin() {
+  // check for horizontal & vertical wins
   for (let i = 0; i < 3; i++) {
     if (
-      gameBoard[0][i] === currentPlayer &&
-      gameBoard[1][i] === currentPlayer &&
-      gameBoard[2][i] === currentPlayer
+      (gameBoard[i][0] === currentPlayer &&
+        gameBoard[i][1] === currentPlayer &&
+        gameBoard[i][2] === currentPlayer) ||
+      (gameBoard[0][i] === currentPlayer &&
+        gameBoard[1][i] === currentPlayer &&
+        gameBoard[2][i] === currentPlayer)
     ) {
       return true;
     }
@@ -39,6 +33,7 @@ function checkWin() {
   ) {
     return true;
   }
+
   if (
     gameBoard[0][2] === currentPlayer &&
     gameBoard[1][1] === currentPlayer &&
@@ -46,6 +41,7 @@ function checkWin() {
   ) {
     return true;
   }
+
   return false;
 }
 
@@ -55,8 +51,23 @@ function handleMove(row, col) {
     cells[row * 3 + col].textContent = currentPlayer;
     console.log(gameBoard);
     if (checkWin()) {
+      cells.forEach((cell) => {
+        cell.removeEventListener("click", handleMoveListener);
+      });
+      setTimeout(() => {
+        noTurn();
+        console.log(currentPlayer + " wins!");
+        modalH1.textContent = `${currentPlayer} wins!`;
+        openModal();
+        replayBtnShow();
+      }, 100);
+    } else if (gameHistory.length === 9) {
+      // check if tie
+      modalH1.textContent = "Draw";
+      openModal();
       replayBtnShow();
-      console.log(currentPlayer + " wins!");
+      noTurn();
+      console.log("tie");
       cells.forEach((cell) => {
         cell.removeEventListener("click", handleMoveListener);
       });
@@ -72,23 +83,32 @@ function handleMove(row, col) {
   }
 }
 
+function noTurn() {
+  console.log("noTurn called");
+  const xturn = document.getElementById("xturn");
+  const oturn = document.getElementById("oturn");
+  xturn.classList.add("opacity-0");
+  oturn.classList.add("opacity-0");
+}
+
 function xTurn() {
   document.getElementById("xturn").classList.remove("opacity-0");
   document.getElementById("oturn").classList.add("opacity-0");
 }
+
 function oTurn() {
   document.getElementById("oturn").classList.remove("opacity-0");
   document.getElementById("xturn").classList.add("opacity-0");
 }
 
-const cells = document.querySelectorAll(".cell");
 const startBtn = document.getElementById("start");
 const resetBtn = document.getElementById("reset");
 startBtn.addEventListener("click", startGame);
 resetBtn.addEventListener("click", resetGame);
 
-let handleMoveListener;
+// main controls
 
+let handleMoveListener;
 function startGame() {
   handleMoveListener = (event) => {
     const cell = event.target;
@@ -97,11 +117,13 @@ function startGame() {
     const col = (parseInt(cellId.substring(5)) - 1) % 3;
     handleMove(row, col);
   };
-
+  startBtn.disabled = true;
+  xTurn();
   cells.forEach((cell) => {
     cell.addEventListener("click", handleMoveListener);
   });
 }
+
 function resetGame() {
   gameBoard = [
     ["", "", ""],
@@ -116,25 +138,28 @@ function resetGame() {
         ["", "", ""],
         ["", "", ""],
       ],
-      move: null,
     },
   ];
-  replayHideAll();
   currentPlayer = "X";
+  xTurn();
   cells.forEach((cell) => {
     cell.textContent = "";
     currentHistoryIndex = 0;
   });
+  replayHideAll();
   startGame();
 }
 
 // game replay
+
 const replayBtn = document.getElementById("replay-button");
 const nextBtn = document.getElementById("next-button");
+const prevBtn = document.getElementById("prev-button");
+
 replayBtn.addEventListener("click", showControls);
 nextBtn.addEventListener("click", nextMove);
-const prevBtn = document.getElementById("prev-button");
 prevBtn.addEventListener("click", previousMove);
+
 function replayHideAll() {
   replayBtn.style.display = "none";
   prevBtn.style.display = "none";
@@ -144,14 +169,21 @@ function replayBtnShow() {
   replayBtn.style.display = "block";
 }
 function showControls() {
+  cells.forEach((cell) => {
+    cell.removeEventListener("click", handleMoveListener);
+  });
   // reset board to first move
   const firstState = gameHistory[1];
   currentMoveIndex = 1;
   updateBoard(firstState);
-  // show controls
+  // show replay controls
   replayBtn.style.display = "none";
+  prevBtn.disabled = true;
+  nextBtn.disabled = false;
   prevBtn.style.display = "block";
   nextBtn.style.display = "block";
+  backdrop.style.display = "none";
+  modal.style.display = "none";
 }
 
 let currentMoveIndex = gameHistory.length - 1;
@@ -188,4 +220,20 @@ function updateBoard() {
       }
     }
   }
+}
+
+// modals
+
+let backdrop = document.querySelector(".backdrop");
+let modal = document.querySelector(".modal");
+let modalH1 = document.querySelector(".modal-h1");
+
+function openModal() {
+  backdrop.style.display = "block";
+  modal.style.display = "flex";
+}
+
+function hideModal() {
+  backdrop.style.display = "none";
+  modal.style.display = "flex";
 }
